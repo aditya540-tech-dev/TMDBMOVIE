12c97eb45115020324fb8559c4929fe1c4b5f36c
aditya540-tech-dev <githubaditya14@gmail.com>
2026-04-10 01:03:42 +0530

    update API base URL

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-API_BASE ="http://127.0.0.1:8000"
+API_BASE = "https://web-production-2c2d2.up.railway.app"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
